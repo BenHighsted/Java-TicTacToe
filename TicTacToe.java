50197c5ea2fa7eeb39b9e6e67c6b8751b01c54c2
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-04-21 00:41:01 +1200

    Basic Setup

---
+++ TicTacToe.java
@@ -0,0 +1,13 @@
+public class TicTacToe{
+    public static void main(String[] args){
+        NewGame();//once you have a GUI make a button 'New Game' that calls this.
+
+    }
+
+    public static void NewGame(){
+        String[] Game = {
+        "null", "null", "null", 
+        "null", "null", "null", 
+        "null", "null", "null"};
+    }
+}

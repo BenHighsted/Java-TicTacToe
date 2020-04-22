e6618d46357c233ad020160dc7c589265316357c
Ben Highsted <49290301+BenHighsted@users.noreply.github.com>
2020-04-22 19:55:23 +1200

    Added a method to check if a win condition has been met
    Checks if any of the eight states that lead to victory have been met.

--- TicTacToe.java
+++ TicTacToe.java
@@ -22,6 +22,7 @@ public class TicTacToe{
         "null", "null", "null", 
         "null", "null", "null", 
         "null", "null", "null"}; //Thinking i'll use this array to hold the state of the game.
+        //With this i can say that GameState[0] == GameState[4] == GameState[8] is a game over (3 in a row)
 
         boolean playerOneTurn = firstMove();
 
@@ -41,14 +42,47 @@ public class TicTacToe{
     }
 
     public static void setUpGUI() {
-        JFrame frame = new JFrame("HelloWorldSwing");
+        JFrame frame = new JFrame("Tic Tac Toe");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(600, 400);
  
-        JLabel label = new JLabel("Hello World");
+        JLabel label = new JLabel("Test Label!");
         frame.getContentPane().add(label);
  
-        frame.pack();
+        //frame.pack();
         frame.setVisible(true);
+    }
+
+    /** Checks the game state to see if a victory condition has been met */
+    public static void checkWinConditions(String[] GameState){
+        /** Rows */
+        if(GameState[0] == GameState[1] && GameState[1] == GameState[2]){//Top row
+
+        }
+        if(GameState[3] == GameState[4] && GameState[4] == GameState[5]){//Middle row
+
+        }
+        if(GameState[6] == GameState[7] && GameState[7] == GameState[8]){//Bottom row
+
+        }
+        /** Columns */
+        if(GameState[0] == GameState[3] && GameState[3] == GameState[6]){//Left column
+
+        }
+        if(GameState[1] == GameState[4] && GameState[4] == GameState[7]){//Middle column
+
+        }
+        if(GameState[2] == GameState[5] && GameState[5] == GameState[8]){//Right column
+
+        }
+        /** Diagonal */
+        if(GameState[0] == GameState[4] && GameState[4] == GameState[8]){//Diagonal (right)
+
+        }
+        if(GameState[2] == GameState[4] && GameState[4] == GameState[6]){//Diagonal (left)
+
+        }
+        
     }
 
 }
